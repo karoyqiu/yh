6ae9a7ec177f17ad85838f5d4a2d05e2eb0fae9d
Roy QIU <karoyqiu@gmail.com>
2024-01-05 10:17:59 +0800

    添加命令行分析。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+  /// The show number
+  #[arg(short, long)]
+  show: i32,
+
+  /// The episode number
+  #[arg(short, long)]
+  episode: i32,
+
+  /// The output directory, default to current directory
+  #[arg(short, long)]
+  directory: Option<String>,
+}
+
+/// 获取当前目录
+fn get_current_dir() -> String {
+  if let Ok(path) = std::env::current_dir() {
+    if let Some(s) = path.to_str() {
+      return s.to_string();
+    }
+  }
+
+  String::default()
+}
+
 fn main() {
-    println!("Hello, world!");
+  let args = Args::parse();
+  let dir = args.directory.unwrap_or(get_current_dir());
+
+  println!(
+    "Downloading episode {} of show {} into {}",
+    args.episode, args.show, &dir
+  );
 }
